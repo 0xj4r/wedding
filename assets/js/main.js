0888f66dbc1e9746b517f52b8f22b650e65f0ba9
Josh Ransom <josh.bh.ransom@gmail.com>
2015-07-19 20:20:11 -0700

    Initial Push

---
+++ assets/js/main.js
@@ -0,0 +1,362 @@
+
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1920px)',
+		normal: '(max-width: 1680px)',
+		narrow: '(max-width: 1280px)',
+		narrower: '(max-width: 1000px)',
+		mobile: '(max-width: 736px)',
+		mobilenarrow: '(max-width: 480px)',
+	});
+$.fn.downCount = function (options, callback) {
+        var settings = $.extend({
+                date: null,
+                offset: null
+            }, options);
+
+        // Throw error if date is not set
+        if (!settings.date) {
+            $.error('Date is not defined.');
+        }
+
+        // Throw error if date is set incorectly
+        if (!Date.parse(settings.date)) {
+            $.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
+        }
+
+        // Save container
+        var container = this;
+
+        /**
+         * Change client's local date to match offset timezone
+         * @return {Object} Fixed Date object.
+         */
+        var currentDate = function () {
+            // get client's current date
+            var date = new Date();
+
+
+            // turn date to utc
+            var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+            console.log(utc);
+            console.log("Timezone : " + date.getTimezoneOffset());
+            // set new Date object
+            var new_date = new Date(utc + (3600000*settings.offset)); 
+            console.log(new_date);
+
+
+            return new_date;
+        };
+
+        /**
+         * Main downCount function that calculates everything
+         */
+        function countdown () {
+            var target_date = new Date(settings.date), // set target date
+                current_date = currentDate(); // get fixed current date
+
+            // difference of dates
+            var difference = target_date - current_date;
+
+            // if difference is negative than it's pass the target date
+            if (difference < 0) {
+                // stop timer
+                clearInterval(interval);
+
+                if (callback && typeof callback === 'function') callback();
+
+                return;
+            }
+
+            // basic math variables
+            var _second = 1000,
+                _minute = _second * 60,
+                _hour = _minute * 60,
+                _day = _hour * 24;
+
+            // calculate dates
+            var days = Math.floor(difference / _day),
+                hours = Math.floor((difference % _day) / _hour),
+                minutes = Math.floor((difference % _hour) / _minute),
+                seconds = Math.floor((difference % _minute) / _second);
+
+                // fix dates so that it will show two digets
+                days = (String(days).length >= 2) ? days : '0' + days;
+                hours = (String(hours).length >= 2) ? hours : '0' + hours;
+                minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
+                seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+
+            // based on the date change the refrence wording
+            var ref_days = (days === 1) ? 'day' : 'days',
+                ref_hours = (hours === 1) ? 'hour' : 'hours',
+                ref_minutes = (minutes === 1) ? 'minute' : 'minutes',
+                ref_seconds = (seconds === 1) ? 'second' : 'seconds';
+
+            // set to DOM
+            container.find('.days').text(days);
+            container.find('.hours').text(hours);
+            container.find('.minutes').text(minutes);
+            container.find('.seconds').text(seconds);
+
+            container.find('.days_ref').text(ref_days);
+            container.find('.hours_ref').text(ref_hours);
+            container.find('.minutes_ref').text(ref_minutes);
+            container.find('.seconds_ref').text(ref_seconds);
+        };
+        
+        // start
+        var interval = setInterval(countdown, 1000);
+    };
+
+// $.fn.countdown = function (callback, message) {
+// 		var duration = 
+//     // If no message is provided, we use an empty string
+//     message = message || "";
+//     // Get reference to container, and set initial content
+//     var container = $(this[0]).html(duration + message);
+//     // Get reference to the interval doing the countdown
+//     var countdown = setInterval(function () {
+//         // If seconds remain
+//         if (--duration) {
+//             // Update our container's message
+//             container.html(duration + message);
+//         // Otherwise
+//         } else {
+//             // Clear the countdown interval
+//             clearInterval(countdown);
+//             // And fire the callback passing our container as `this`
+//             callback.call(container);   
+//         }
+//     // Run interval every 1000ms (1 second)
+//     }, 1000);
+
+// };
+
+// // Use p.countdown as container, pass redirect, duration, and optional message
+// $(".countdown").countdown(redirect, 5, "s remaining");
+
+// // Function to be called after 5 seconds
+// function redirect () {
+//     this.html("Done counting, redirecting.");
+//     window.location = "http://msdn.microsoft.com";
+// }
+
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$all = $body.add($header);
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 0);
+			});
+
+		// Touch mode.
+			skel.on('change', function() {
+
+				if (skel.vars.mobile || skel.breakpoint('mobile').active)
+					$body.addClass('is-touch');
+				else
+					$body.removeClass('is-touch');
+
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Gallery.
+			$window.on('load', function() {
+				$('.gallery').poptrox({
+					baseZIndex: 10001,
+					useBodyOverflow: false,
+					usePopupEasyClose: false,
+					overlayColor: '#1f2328',
+					overlayOpacity: 0.65,
+					usePopupDefaultStyling: false,
+					usePopupCaption: true,
+					popupLoaderText: '',
+					windowMargin: (skel.breakpoint('mobile').active ? 5 : 50),
+					usePopupNav: true
+				});
+			});
+
+		// Section transitions.
+			if (!skel.vars.mobile
+			&&	skel.canUse('transition')) {
+
+				var on = function() {
+
+					// Generic sections.
+						$('.main.style1')
+							.scrollex({
+								mode:		'middle',
+								delay:		100,
+								initialize:	function() { $(this).addClass('inactive'); },
+								terminate:	function() { $(this).removeClass('inactive'); },
+								enter:		function() { $(this).removeClass('inactive'); },
+								leave:		function() { $(this).addClass('inactive'); }
+							});
+
+						$('.main.style2')
+							.scrollex({
+								mode:		'middle',
+								delay:		100,
+								initialize:	function() { $(this).addClass('inactive'); },
+								terminate:	function() { $(this).removeClass('inactive'); },
+								enter:		function() { $(this).removeClass('inactive'); },
+								leave:		function() { $(this).addClass('inactive'); }
+							});
+
+					// Work.
+						$('#work')
+							.scrollex({
+								top:		'40vh',
+								bottom:		'30vh',
+								delay:		50,
+								initialize:	function() {
+
+												var t = $(this);
+
+												t.find('.row.images')
+													.addClass('inactive');
+
+											},
+								terminate:	function() {
+
+												var t = $(this);
+
+												t.find('.row.images')
+													.removeClass('inactive');
+
+											},
+								enter:		function() {
+
+												var t = $(this),
+													rows = t.find('.row.images'),
+													length = rows.length,
+													n = 0;
+
+												rows.each(function() {
+													var row = $(this);
+													window.setTimeout(function() {
+														row.removeClass('inactive');
+													}, 100 * (length - n++));
+												});
+
+											},
+								leave:		function(t) {
+
+												var t = $(this),
+													rows = t.find('.row.images'),
+													length = rows.length,
+													n = 0;
+
+												rows.each(function() {
+													var row = $(this);
+													window.setTimeout(function() {
+														row.addClass('inactive');
+													}, 100 * (length - n++));
+												});
+
+											}
+							});
+
+					// Contact.
+						$('#contact')
+							.scrollex({
+								top:		'50%',
+								delay:		50,
+								initialize:	function() { $(this).addClass('inactive'); },
+								terminate:	function() { $(this).removeClass('inactive'); },
+								enter:		function() { $(this).removeClass('inactive'); },
+								leave:		function() { $(this).addClass('inactive'); }
+							});
+
+				};
+
+				var off = function() {
+
+					// Generic sections.
+						$('.main.style1')
+							.unscrollex();
+
+						$('.main.style2')
+							.unscrollex();
+
+					// Work.
+						$('#work')
+							.unscrollex();
+
+					// Contact.
+						$('#contact')
+							.unscrollex();
+
+				};
+
+				skel.on('change', function() {
+
+					if (skel.breakpoint('mobile').active)
+						(off)();
+					else
+						(on)();
+
+				});
+
+			}
+
+		// Events.
+			var resizeTimeout, resizeScrollTimeout;
+
+			$window
+				.resize(function() {
+
+					// Disable animations/transitions.
+						$body.addClass('is-resizing');
+
+					window.clearTimeout(resizeTimeout);
+
+					resizeTimeout = window.setTimeout(function() {
+
+						// Update scrolly links.
+							$('a[href^=#]').scrolly({
+								speed: 1500,
+								offset: $header.outerHeight() - 1
+							});
+
+						// Re-enable animations/transitions.
+							window.setTimeout(function() {
+								$body.removeClass('is-resizing');
+								$window.trigger('scroll');
+							}, 0);
+
+					}, 100);
+
+				})
+				.load(function() {
+					$window.trigger('resize');
+				});
+
+	});
+
+})(jQuery);
